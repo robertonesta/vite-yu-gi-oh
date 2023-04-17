934560f6cf17c228e02add2f1989d151f2d250e9
RobertoNesta <robertonesta1993@gmail.com>
2023-04-17 15:48:38 +0200

    new components created

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,18 +1,35 @@
-<template>
-   
-</template>
-
 <script>
+import CardsFound from "./CardsFound.vue"
+import CardsList from "./CardsList.vue"
+
 export default {
     name: "AppMain",
-    setup () {
-        
-
-        return {}
+    components: {
+        CardsFound,
+        CardsList,
     }
+
 }
 </script>
 
+<template>
+    <div class="sand-bg">
+        <div class="container py-3">
+            <select class="form-select-m rounded" >
+                <option selected>Choose the archetype</option>
+                <option value="1">Alien</option>
+            </select>
+        </div>
+        <div class="container py-3 bg-light">
+            <CardsFound/>
+            <CardsList/>
+        </div>
+    </div>   
+</template>
+
 <style lang="scss" scoped>
+.sand-bg{
+    background-color: #d48f38;
+}
 
 </style>
---
+++ src/components/CardsFound.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        name: "CardsFound",
+    }
+</script>
+
+<template>
+    <div class="dark-bg text-white p-2">
+        <span>Found x cards</span>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.dark-bg{
+    background-color: #212529;
+}
+
+</style>
